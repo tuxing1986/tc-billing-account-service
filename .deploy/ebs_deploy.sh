06099e92ec46949ef005583e37a0c7faeedfc09e
Guiqiang Zhang <skyhit@gmail.com>
2017-02-08 11:45:16 +0800

    need to init the eb directory

--- .deploy/ebs_deploy.sh
+++ .deploy/ebs_deploy.sh
@@ -33,6 +33,7 @@ export AWS_ACCESS_KEY_ID=$(eval "echo \$${ENV}_AWS_ACCESS_KEY_ID")
 export AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
 
 # eb deploy
+eb init -r us-east-1 $SERVICE
 cd .deploy
 EB_OUTPUT="$(eb deploy -l $TAG -r us-east-1)"
 echo $EB_OUTPUT
