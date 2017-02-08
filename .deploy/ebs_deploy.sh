99a8b558cb65ead09527ea7872ca178f64f94643
Guiqiang Zhang <skyhit@gmail.com>
2017-02-08 11:56:06 +0800

    call eb init in .deploy directory

--- .deploy/ebs_deploy.sh
+++ .deploy/ebs_deploy.sh
@@ -33,8 +33,8 @@ export AWS_ACCESS_KEY_ID=$(eval "echo \$${ENV}_AWS_ACCESS_KEY_ID")
 export AWS_SECRET_ACCESS_KEY=$(eval "echo \$${ENV}_AWS_SECRET_ACCESS_KEY")
 
 # eb deploy
-eb init -r us-east-1 $SERVICE
 cd .deploy
+eb init -r us-east-1 $SERVICE
 EB_OUTPUT="$(eb deploy -l $TAG -r us-east-1)"
 echo $EB_OUTPUT
 if [[ $EB_OUTPUT =~ .*ERROR.* ]]
